48ff0186822601bb4f8d64a661d30972b323ed7f
yusrah <yusrahkhan06@gmail.com>
2024-07-31 18:47:52 +0200

    first commit

---
+++ gs-01-starting-project/app.js
@@ -0,0 +1,27 @@
+Vue.createApp ({
+    data() {
+        return {
+            goals: [],
+            entered: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
+// const buttonEL = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal(){
+//     const enteredValue = inputEl.value;
+//     const listItemEL = document.createElement('li');
+//     listItemEL.textContent = enteredValue;
+//     listEl.appendChild(listItemEL);
+//     inputEl.value = '';
+// }
+
+// buttonEL.addEventListener('click',addGoal );
